a790b74a1bcc0af80267463e061c6fbe046b5ed3
Markus Min <markus.min@gmail.com>
2021-02-02 19:56:41 -0800

    Added heatmap to CPUE

--- analysis/PW_CPUE_anaylsis.R
+++ analysis/PW_CPUE_anaylsis.R
@@ -160,6 +160,121 @@ PW_nboc <- dim(PW_survey_boc)[1]
 PW_bocaccio_nominal_CPUE_angler_days <- PW_nboc/PW_angler_days
 PW_bocaccio_nominal_CPUE_angler_days
 
+## ---CREATE HEATMAP of effort--------------------------------------------------
+
+
+## ----Base map of Puget sound--------------------------------------------------
+
+# Load data
+usa_spdf <- readOGR(dsn = here("map_files", "USA_adm0.shp"))
+
+# Convert to df(ish)
+usa_spdf_fort <- tidy(usa_spdf)
+
+# Load BC data
+BC_shp <- readOGR(dsn = here("map_files", "canada", "lpr_000b16a_e.shp"))
+# proj4string(BC_shp)
+BC_shp_transform <- spTransform(BC_shp, "+init=epsg:4326")
+BC_spdf_fort <- tidy(BC_shp_transform)
+
+
+# Create base map in ggplot
+greater_PS_map <- ggplot(usa_spdf_fort, aes(x = long, y = lat, group = group))+
+  # Create grid using geom_vline and geom_hline
+  # geom_vline(xintercept = seq(-123.1, min_lon, 1/nm), color = "gray50", size = 0.1)+
+  # geom_hline(yintercept = seq(min_lat, 48.5, 1/60), color = "gray50", size = 0.1)+
+  #base map
+  geom_polygon(color = "gray20", fill = "gray20")+
+  geom_polygon(data = BC_spdf_fort, aes(x = long, y = lat, group = group), inherit.aes = FALSE) +
+  ylab("Latitude")+
+  coord_fixed(ylim = c(47.1,48.8),  xlim = c(-124.7,-122.1), ratio = 1.3)+
+  theme(plot.background = element_rect(fill = "white"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+
+## ---- Prepare data for map ---------------------------------------------------
+
+## Load data
+PW_survey_path <- here("hook_and_line_data","Washington_et_al_74-77_Puget_Sound_data.xlsx")
+excel_sheets(PW_survey_path)
+PW_survey <- read_excel(PW_survey_path, sheet = "catch_species_names")
+
+# Investigate locations that don't have a letter/number format
+# as.data.frame(subset(PW_survey, Location %in% c("485", "490", "491", "492")))
+# All coppers
+
+# Determine whether letters or numbers correspond to lat/long
+
+# Extract letters/numbers
+PW_survey %>%
+  mutate(letter_loc = gsub("[^a-zA-Z]", "", Location)) %>%
+  mutate(number_loc = gsub("\\D", "", Location)) %>%
+  mutate(number_loc_numeric = as.numeric(number_loc)) -> PW_survey
+
+# match(substring(PW_survey$letter_loc,1,1),LETTERS[1:26])*nchar(PW_survey$letter_loc)
+
+# Convert letters to numeric
+# Match letters to order in alphabet, then add 26 for each additional letter in string
+# Account for the fact that letters start at B, not A (hence -1 at the end)
+PW_survey %>%
+  mutate(letter_loc_numeric = match(substring(letter_loc,1,1),
+                                    LETTERS[1:26])+(nchar(letter_loc)-1)*26-1) -> PW_survey
+
+# Adjust the letters to be positive east of Point Monroe (M78) and negative west of it
+# Adjust the numbers to be positive north of Point Monroe (M78) and negative south of it
+PW_survey %>% 
+  mutate(letter_loc_numeric = letter_loc_numeric-12) %>% 
+  mutate(number_loc_numeric = number_loc_numeric - 78) -> PW_survey
+
+sort(unique(PW_survey$letter_loc))
+sort(unique(PW_survey$number_loc))
+
+# Calculate distance of longitude at this latitude
+lon_miles <- cos(47.66*pi/180) * 69.172
+nm <- lon_miles/1.151
+
+# Starting point is Point Monroe (M78)
+point_monroe_lat <- 47.7088
+point_monroe_lon <- -122.5115
+
+# Calculate lat/lon using M78 as anchoring point
+PW_survey %>%
+  mutate(lat_est = point_monroe_lat - number_loc_numeric/60) %>%
+  mutate(lon_est = point_monroe_lon - letter_loc_numeric/nm) -> PW_survey
+
+PW_effort <- left_join(PW_effort, PW_survey_locations, by = "Location")
+
+
+
+
+## --Plot map of effort (angler hours)------------------------------------------
+# Set figure directory
+fig_dir <- here("figures", "map_figures")
+
+PW_effort_map <- greater_PS_map + stat_summary_2d(data = PW_effort, aes(x = lon_est, y = lat_est, z = angler_time/3600),
+                                                  binwidth = c(0.02, 0.02),inherit.aes = FALSE)+
+  scale_fill_gradientn(limits=c(0,30), breaks=seq(0, 30, by=5), colours=c("#ffffb2", "#fed976", "#feb24c", "#fd8d3c", 
+                                                                          "#fc4e2a", "#e31a1c", "#b10026"))+
+  theme(legend.title = element_text(size = 14),
+        legend.text = element_text(size = 12),
+        axis.text = element_text(size = 12))+
+  labs(fill = "Angler Hours")+
+  xlab("longitude")
+
+ggsave(paste0(fig_dir,"/PW_effort_map.png"),PW_effort_map, width = 12, height = 10)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
